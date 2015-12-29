0292cbd7203e71c57931d33ef255c5e582924e00
TNMEM <louis_a_king@hotmail.com>
2015-12-29 01:30:48 +0000

    nodemon and tweaks

--- routes/xsequelize.js
+++ routes/xsequelize.js
@@ -1,15 +1,14 @@
 var store = require('./store');
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize('TESTONLY', 'TESTONLY', 'TESTONLY', {
+var sq = new Sequelize('TESTONLY', 'TESTONLY', 'TESTONLY', {
   host: "awsmysql.cuuagsmfyvhg.us-east-1.rds.amazonaws.com",
   port: 3306,
   logging: false, // false or a special function.
   dialect: 'mysql'
 });
-sequelize.query("SELECT * from nj_items").success(function(myTableRows) {
-  console.log(myTableRows)
+
+sq.query("SELECT * FROM `nj_items`", { type: Sequelize.QueryTypes.SELECT}).then(function(myTableRows) {
+  console.log(myTableRows);
   store.theItems = myTableRows;
-}).error(function(errors) {
-  console.log(errors)
 });
